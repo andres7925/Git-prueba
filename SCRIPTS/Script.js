beeb0fa1931b7b41011a82bc1d3432cd5e3add7c
Andres <andresfelipe.gomez@utp.edu.co>
2024-01-01 22:39:03 -0500

    primer commit

---
+++ SCRIPTS/Script.js
@@ -0,0 +1,10 @@
+const boton = document.getElementById("Dirigir");
+
+function MostrarCartelDirigir(){
+    boton.innerHTML = "Dio click en Dirigir";
+}
+
+function MostrarCartelCancelar(){
+    boton.innerHTML = "Dio click en Cancelar";
+}
+
